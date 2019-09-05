9c3e2a79d38565907d2f6ad38e2bb183ee55efa6
Julian Brummer <jbrummer@students.uni-mainz.de>
2019-09-05 12:40:29 +0200

    added all include file pmr.h

---
+++ include/pmr/pmr.h
@@ -0,0 +1,16 @@
+#ifndef PMR_H
+#define PMR_H
+
+#include "deque.h"
+#include "forward_list.h"
+#include "list.h"
+#include "map.h"
+#include "memory_resource.h"
+#include "regex.h"
+#include "set.h"
+#include "string.h"
+#include "unordered_map.h"
+#include "unordered_set.h"
+#include "vector.h"
+
+#endif //PMR_H
